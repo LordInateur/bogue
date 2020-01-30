5dedda080485bd4ff9b65d989c6739ac4b575e47
Matthieu Fournier <mat.fournier85@gmail.com>
2020-01-30 22:55:15 +0100

    Init project : React / Bootstrap / Sass / Typescript

---
+++ src/App/App.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link disabled" href="#" aria-disabled="true">Disabled</a>
+            </li>
+          </ul>
+        </div>
+      </nav>  
+
+      
+      {/* <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn React
+      </a> */}
+        
+      </header>
+
+      <div>coucou</div>
+    </div>
+  );
+}
+
+export default App;
